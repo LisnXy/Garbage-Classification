2fe25160b69e8ecc9d49e59826f4a6c337987827
LisnX <53891402+LisnXy@users.noreply.github.com>
2022-04-12 10:05:23 +0800

    FIX
    * fix 拍照页面时，当图片过长时会撑开容器
    * 用户界面的部分样式变动

--- garbage-sort-web/components/tabBar/tabBar.vue
+++ garbage-sort-web/components/tabBar/tabBar.vue
@@ -1,123 +1,115 @@
 <template>
-    <view class="tabBar-root">
-        <view class="tabBar-item" @click="switchTab(0)">
-            <image
-                :src="iconPath(0)"
-                mode="scaleToFill"
-                class="aside-image"
-            ></image>
-            <text>主页</text>
-        </view>
-        <view class="tabBar-item" @click="switchTab(1)">
-            <image
-                :src="iconPath(1)"
-                mode="scaleToFill"
-                class="mid-btn"
-            ></image>
-            <text style="z-index: 2000; position: relative; top: 2vh">
-                搜索
-            </text>
-        </view>
-        <view class="tabBar-item" @click="switchTab(2)">
-            <image
-                :src="iconPath(2)"
-                mode="scaleToFill"
-                class="aside-image"
-            ></image>
-            <text>个人中心</text>
-        </view>
-    </view>
+	<view class="tabBar-root">
+		<view class="tabBar-item" @click="switchTab(0)">
+			<image :src="iconPath(0)" mode="scaleToFill" class="aside-image"></image>
+			<text>主页</text>
+		</view>
+		<view class="tabBar-item" @click="switchTab(1)">
+			<image :src="iconPath(1)" mode="scaleToFill" class="mid-btn"></image>
+			<text style="z-index: 2000; position: relative; top: 2vh">
+				搜索
+			</text>
+		</view>
+		<view class="tabBar-item" @click="switchTab(2)">
+			<image :src="iconPath(2)" mode="scaleToFill" class="aside-image"></image>
+			<text>个人中心</text>
+		</view>
+	</view>
 </template>
 
 <script>
-export default {
-    name: 'tabBar',
-    data() {
-        return {
-            list: [
-                {
-                    pagePath: '../../pages/sortInfo_home/sortInfo_home',
-                    iconPath: '../../static/icons/home_off.svg',
-                    selectedIconPath: '../../static/icons/home_on.svg',
-                    text: '主页'
-                },
-                {
-                    pagePath: '../../pages/upload_photo/uploadphototest',
-                    iconPath: '../../static/icons/Search_off.svg',
-                    selectedIconPath: '../../static/icons/Search_on.svg',
-                    text: '搜索'
-                },
-                {
-                    pagePath: '../../pages/userPage/userPage',
-                    iconPath: '../../static/icons/user_off.svg',
-                    selectedIconPath: '../../static/icons/account_on.svg',
-                    text: '个人中心'
-                }
-            ]
-        };
-    },
-    props: {
-        selectedIndex: Number
-    },
-    computed: {},
-    methods: {
-        switchTab(itemIndex) {
-            uni.switchTab({
-                url: this.list[itemIndex].pagePath,
-                fail(err) {
-                    console.log(err);
-                }
-            });
-        },
-        iconPath(itemIndex) {
-            if (this.selectedIndex === itemIndex) {
-                return this.list[itemIndex].selectedIconPath;
-            } else {
-                return this.list[itemIndex].iconPath;
-            }
-        }
-    }
-};
+	export default {
+		name: 'tabBar',
+		data() {
+			return {
+				list: [{
+						pagePath: '../../pages/sortInfo_home/sortInfo_home',
+						iconPath: '../../static/icons/home_off.svg',
+						selectedIconPath: '../../static/icons/home_on.svg',
+						text: '主页'
+					},
+					{
+						pagePath: '../../pages/upload_photo/uploadphototest',
+						iconPath: '../../static/icons/Search_off.svg',
+						selectedIconPath: '../../static/icons/Search_on.svg',
+						text: '搜索'
+					},
+					{
+						pagePath: '../../pages/userPage/userPage',
+						iconPath: '../../static/icons/user_off.svg',
+						selectedIconPath: '../../static/icons/account_on.svg',
+						text: '个人中心'
+					}
+				]
+			};
+		},
+		props: {
+			selectedIndex: Number
+		},
+		computed: {},
+		methods: {
+			switchTab(itemIndex) {
+				uni.switchTab({
+					url: this.list[itemIndex].pagePath,
+					success: () => {
+						this.$emit('switch-tab', itemIndex);
+					},
+					fail(err) {
+						console.log(err);
+					}
+				});
+			},
+			iconPath(itemIndex) {
+				if (this.selectedIndex === itemIndex) {
+					return this.list[itemIndex].selectedIconPath;
+				} else {
+					return this.list[itemIndex].iconPath;
+				}
+			}
+		}
+	};
 </script>
 
 <style lang="less">
-.tabBar-root {
-    background-color: #fff;
-    width: 100%;
-    align-self: flex-end;
-    display: flex;
-    justify-content: space-around;
-    height: 100px;
-}
+	.tabBar-root {
+		background-color: #fff;
+		width: 100%;
+		align-self: flex-end;
+		display: flex;
+		justify-content: space-around;
+		height: 100px;
+	}
 
-.tabBar-item {
-    position: relative;
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    image {
-        width: 33px;
-        height: 33px;
-    }
-    text {
-        display: block;
-    }
-}
+	.tabBar-item {
+		position: relative;
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
 
-.mid-btn {
-    border-radius: 50%;
-    width: 60px !important;
-    height: 60px !important;
-    position: absolute;
-    bottom: 40%;
-    background-color: #fff;
-    padding: 0.7rem;
-}
+		image {
+			width: 33px;
+			height: 33px;
+		}
 
-.aside-image {
-    position: relative;
-    bottom: 10px;
-}
+		text {
+			display: block;
+		}
+	}
+
+	.mid-btn {
+		border-radius: 50%;
+		width: 60px !important;
+		height: 60px !important;
+		position: absolute;
+		bottom: 40%;
+		background-color: #fff;
+		padding: 0.7rem;
+	}
+
+	.aside-image {
+		position: relative;
+		bottom: 10px;
+	}
 </style>
